412443c41b46ddbad40c9f7648d35e63d11f0e1b
jereMYltar <jereMYltar@users.noreply.github.com>
2022-02-21 21:39:08 +0100

    corrections CROS et ajout d'un middleware dans la route de suppression

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -10,7 +10,7 @@ router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, rightscheck, multer, sauceCtrl.modifySauce);
-router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.delete('/:id', auth, rightscheck, sauceCtrl.deleteSauce);
 router.post('/:id/like', auth,sauceCtrl.likeSauce);
 
 
